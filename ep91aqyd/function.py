47811a21a26a47042a6f40fa7763ea99bca7edf0
SaiVarunVaranasi <saivarunvaranasi@gmail.com>
2021-11-12 14:53:40 +0100

    Initial Commit

---
+++ ep91aqyd/function.py
@@ -0,0 +1,12 @@
+import numpy as np
+from ipywidgets import interact, fixed
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def imshow(X, resize=None):
+    img = Image.open(X)
+    img = np.resize(X,resize)
+    imgplot = plt.imshow(img)
+    plt.show()
+    return imgplot
