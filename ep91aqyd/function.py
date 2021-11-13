23d840eacbd0acdd59ef6f5f050b011a3327aca6
Sai Varun Varanasi <82500909+SaiVarunVaranasi@users.noreply.github.com>
2021-11-13 20:16:51 +0100

    update function.py

--- ep91aqyd/function.py
+++ ep91aqyd/function.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def imshow(X, resize):
-    arr = np.load('img.npy')
+    arr = np.load(X)
     img = Image.fromarray(arr)
     img.resize(size=(100, 100))
     fig, ax = plt.subplots()
