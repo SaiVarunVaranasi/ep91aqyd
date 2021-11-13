8bd826b75d77f8b9b422a5093c454f794e052d55
Sai Varun Varanasi <82500909+SaiVarunVaranasi@users.noreply.github.com>
2021-11-13 22:31:14 +0100

    Decoding error fix

--- ep91aqyd/function.py
+++ ep91aqyd/function.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 def imshow(X, resize):
     file = open(X)
-    img = np.load(file, "rb").decode(errors='ignore')
+    img = np.load(str(file), 'r')
     file.close()
     img = np.resize(img, resize)
     fig, ax = plt.subplots()
