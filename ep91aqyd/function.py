8e67ac389124b20f58f20875d644f294c2d15733
Sai Varun Varanasi <82500909+SaiVarunVaranasi@users.noreply.github.com>
2021-11-13 20:10:09 +0100

    Update function.py

--- ep91aqyd/function.py
+++ ep91aqyd/function.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-def imshow(X, resize=None):
+def imshow(X, resize):
     img = cv2.imread(X)
     img = cv2.resize(img, dsize=resize, interpolation=cv2.INTER_CUBIC)
     fig, ax = plt.subplots()
