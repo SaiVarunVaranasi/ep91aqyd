94611a3c3cf2408280af10e3d2e4da22c490e7b1
Sai Varun Varanasi <82500909+SaiVarunVaranasi@users.noreply.github.com>
2021-11-13 19:37:08 +0100

    Updated function.py

--- ep91aqyd/function.py
+++ ep91aqyd/function.py
@@ -1,12 +1,9 @@
 import numpy as np
-from ipywidgets import interact, fixed
-from PIL import Image
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
 def imshow(X, resize=None):
     img = Image.open(X)
-    img = np.resize(X,resize)
-    plt.imshow(img)
+    img = np.resize(img,resize)
+    fig, ax = plt.subplots()
+    ax.imshow(img, cmap='jet', interpolation='nearest')
     plt.show()
-    
