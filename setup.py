740427f804f31f509e7ae1804c2148e30223130e
Sai Varun Varanasi <82500909+SaiVarunVaranasi@users.noreply.github.com>
2021-11-13 22:33:40 +0100

    Version Update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 from setuptools import find_packages
 setup(name='ep91aqyd',
-version='0.1.12',
+version='0.1.13',
 author='DSSS',
 author_email='varun.sai.varanasi@fau.de',
 packages=find_packages(),
