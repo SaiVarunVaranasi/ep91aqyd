78c1d3de6a0adf6a3baf22f342f1a311e0250586
Sai Varun Varanasi <82500909+SaiVarunVaranasi@users.noreply.github.com>
2021-11-13 19:38:10 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 from setuptools import find_packages
 setup(name='ep91aqyd',
-version='0.1',
+version='0.1.1',
 author='DSSS',
 author_email='varun.sai.varanasi@fau.de',
 packages=find_packages(),
-install_requires=['numpy', 'Pillow', 'ipywidgets', 'matplotlib'])
+install_requires=['numpy', 'matplotlib'])
