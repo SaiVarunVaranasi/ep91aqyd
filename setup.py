553ed284b59a56232f4b98b18af0f4e2604eee51
Sai Varun Varanasi <82500909+SaiVarunVaranasi@users.noreply.github.com>
2021-11-13 20:39:46 +0100

    replaced pillow
    Pillow is throwing errors while running the code

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 from setuptools import find_packages
 setup(name='ep91aqyd',
-version='0.1.8',
+version='0.1.9',
 author='DSSS',
 author_email='varun.sai.varanasi@fau.de',
 packages=find_packages(),
-install_requires=['numpy','Pillow', 'matplotlib'])
+install_requires=['numpy', 'matplotlib'])
