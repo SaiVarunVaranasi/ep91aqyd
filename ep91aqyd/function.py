28aa007d402735aba40acafaa81cd0ffc3a4f80b
Sai Varun Varanasi <82500909+SaiVarunVaranasi@users.noreply.github.com>
2021-11-13 22:45:53 +0100

    update function.py

--- ep91aqyd/function.py
+++ ep91aqyd/function.py
@@ -1,10 +1,10 @@
 import numpy as np
+from ipywidgets import interact, fixed
+from PIL import Image
 import matplotlib.pyplot as plt
 
 def imshow(X, resize):
-    file = open(X, "r")
-    img = np.load(file)
-    file.close()
+    img = Image.open("image_path.jpg")
     img = np.resize(img, resize)
     fig, ax = plt.subplots()
     ax.imshow(img)
