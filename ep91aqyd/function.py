97135aa3251fd9ff7f5beff535e970d2df47290d
Sai Varun Varanasi <82500909+SaiVarunVaranasi@users.noreply.github.com>
2021-11-13 22:35:46 +0100

    Encoding error

--- ep91aqyd/function.py
+++ ep91aqyd/function.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def imshow(X, resize):
-    file = open(X)
-    img = np.load(str(file), "r")
+    file = open(X, "r")
+    img = np.load(file)
     file.close()
     img = np.resize(img, resize)
     fig, ax = plt.subplots()
